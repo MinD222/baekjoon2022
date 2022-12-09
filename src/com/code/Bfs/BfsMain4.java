d849450a4609089f7bb6fb21dd28cf12afc9df8d
mjnzy <minji_jeong@naver.com>
2022-12-09 20:20:21 +0900

    2178. 미로탐색

--- src/com/code/Bfs/BfsMain4.java
+++ src/com/code/Bfs/BfsMain4.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 
 /*
 2178. 미로탐색
+queue 에 배열을 넣는 이유.. 0,0 > 1,0 등으로 x, y 에 따라 사방향???
 4 6
 101111
 101010
@@ -19,26 +20,62 @@ public class BfsMain4 {
     static int N,M;
     static int[][] graph;
     static int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
-    static Queue queue = new LinkedList();
+    static Queue<int[]> queue = new LinkedList();
+    //static Queue<Integer> queue = new LinkedList();
+    static boolean[][] visited;
+    static int cnt=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        graph = new int[N][M];
+        visited = new boolean[N][M];
+
         // 미로 만들기(인접행렬)
         for(int i=0;i<N;i++){
-            String line = st.nextToken();
+            String line = br.readLine();
             for(int j=0;j<M;j++){
-                graph[i][j]=line.charAt(i)-'0';
+                graph[i][j]=line.charAt(j)-'0';
             }
         }
-
-        bfs();
+/*
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(!visited[i][j]) {
+                    bfs(i, j);
+                }
+            }
+        }*/
+        bfs(0, 0);
+        System.out.println(graph[N-1][M-1]);
     }
 
-    static void bfs(){
+    static void bfs(int x, int y){
+       // queue.offer(graph[x][y]);
+        queue.offer(new int[]{0,0});
+        visited[x][y]=true;
+       // graph[x][y]=cnt;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            //int now = queue.poll();
+            for (int[] dir : dirs) {
 
+                int nextX = dir[0] + now[0];
+               // int nextX = dir[0] + x;
+                int nextY = dir[1] + now[1];
+               // int nextY = dir[1] + y;
+                if (nextX >= 0 && nextY >= 0 && nextX < N && nextY < M && !visited[nextX][nextY] && graph[nextX][nextY] == 1) {
+                   // cnt++;
+                    visited[nextX][nextY]=true;
+                    graph[nextX][nextY] = graph[now[0]][now[1]] + 1;
+                 //   graph[nextX][nextY] = now + 1;
+                    queue.offer(new int[]{nextX,nextY});
+                  //  queue.offer(graph[nextX][nextY]);
+                }
+            }
+        }
 
     }
 }
